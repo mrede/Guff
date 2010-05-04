9131d984916da10b666e085b8bfbe59bb791449d
Ben <ben@bens-macbook.local>
2010-05-04 20:40:55 +0100

    back to normal

--- web/js/geo.js
+++ web/js/geo.js
@@ -8,6 +8,7 @@ function geoHandler(location) {
 	$("#post_latitude").attr("value", location.coords.latitude);
 	$("#post_accuracy").attr("value", location.coords.accuracy);
 	$("#submit_but").removeAttr("disabled");
+	console.log("YUP");
 	
 	//get image
 	$("#img").attr("src", "http://maps.google.com/maps/api/staticmap?center="+location.coords.latitude+","+location.coords.longitude+"&zoom=15&size=150x150&maptype=roadmap&markers=color:blue|"+location.coords.latitude+","+location.coords.longitude+"&sensor=false")
@@ -34,11 +35,12 @@ var guff_geo = {
 
 	init:function() {	
 		$("#form_div").hide();
-	//	navigator.geolocation.getCurrentPosition(geoHandler, errorHandler, {
-	//		enableHighAccuracy: true
-//		});
+		navigator.geolocation.getCurrentPosition(geoHandler, errorHandler, {
+			enableHighAccuracy: true,
+			maximumAge: 60
+		});
 		
-		navigator.geolocation.watchPosition(updatePos);
+	
 	}
 	
 	
