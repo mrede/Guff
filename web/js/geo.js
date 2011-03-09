01c3b93c30332152a368e812c195bac74006093a
Ben Ede <ben.ede@gmail.com>
2011-03-09 20:58:53 +0000

    tidied up, test is turned on

--- web/js/geo.js
+++ web/js/geo.js
@@ -1,55 +1,4 @@
-var watchId;
-
-function geoHandler(location) {
-	//Check acc before bothering
-//	console.log("Location", location);
-	if (location.coords.accuracy<50)
-	{
-		//cancel watch
-		navigator.geolocation.clearWatch(watchId);
-	}
-	if (location.coords.accuracy<100)
-	{
-		
-		$("#post_longitude").attr("value", location.coords.longitude);
-		$("#post_latitude").attr("value", location.coords.latitude);
-		$("#post_accuracy").attr("value", location.coords.accuracy);
-		$("#submit_but").removeAttr("disabled");
-	
-		//get image
-		$("#map_img").attr("src", "http://maps.google.com/maps/api/staticmap?center="+location.coords.latitude+","+location.coords.longitude+"&zoom=15&size=400x300&maptype=roadmap&markers=color:blue|"+location.coords.latitude+","+location.coords.longitude+"&sensor=false").load(function() {
-			$(this).fadeIn();
-			$('#ajax_loader').hide();
-			$("#form_holder").slideDown();
-		});
-		
-		//Get messages
-		$("#messages").load("/post/messages/"+location.coords.latitude+"/"+location.coords.longitude);
-	
-		$("#dump").html("Lat: "+location.coords.latitude+", Lng: "+location.coords.longitude+", Acc:"+location.coords.accuracy)
-	}
-	else
-	{
-		$("#dump").append('.');
-	}
-}
-
-function updatePos(location)
-{
-	$('#form_holder').slideDown();
-	guff_geo.loadMap();
-	//console.log("Position located", "Lat: "+location.coords.latitude+", Lng: "+location.coords.longitude+", Acc:"+location.coords.accuracy);
-	$("#debug").append("Lat: "+location.coords.latitude+", Lng: "+location.coords.longitude+", Acc:"+location.coords.accuracy)
-	$('#map_img').attr('src', 'image.jpg').load(function() {
-	alert('Image Loaded');
-	});
-}
-
-function errorHandler(err) {
-	//try again
-	alert("ERROR"+err);
-}
-var test = 0;
+var test = 1;
 
 var guff_geo = {
 	
@@ -62,18 +11,68 @@ var guff_geo = {
 		});
 	},
 	
+	geoHandler:function(location) {
+    	//Check acc before bothering
+    //	console.log("Location", location);
+    	if (location.coords.accuracy<50)
+    	{
+    		//cancel watch
+    		navigator.geolocation.clearWatch(watchId);
+
+    	}
+    	if (location.coords.accuracy<100)
+    	{
+    		$("#post_longitude").attr("value", location.coords.longitude);
+    		$("#post_latitude").attr("value", location.coords.latitude);
+    		$("#post_accuracy").attr("value", location.coords.accuracy);
+    		$("#submit_but").removeAttr("disabled");
+
+    		//get image
+    		$("#map_img").attr("src", "http://maps.google.com/maps/api/staticmap?center="+location.coords.latitude+","+location.coords.longitude+"&zoom=15&size=400x300&maptype=roadmap&markers=color:blue|"+location.coords.latitude+","+location.coords.longitude+"&sensor=false").load(function() {
+    			$(this).fadeIn();
+    			$('#ajax_loader').hide();
+    			$("#form_holder").slideDown();
+    		});
+            
+    		//Get messages
+    		$("#messages").load("/post/messages/"+location.coords.latitude+"/"+location.coords.longitude);
+
+    		$("#dump").html("Lat: "+location.coords.latitude+", Lng: "+location.coords.longitude+", Acc:"+location.coords.accuracy)
+    		
+    		navigator.geolocation.watchPosition(guff_geo.geoHandler, guff_geo.errorHandler, {
+    			enableHighAccuracy: true,
+    			maximumAge: 0
+    		});
+    	}
+    	else
+    	{
+    		//still waiting
+    	}
+    },
+    
+    errorHandler:function(err) {
+    	//try again
+    	if (err.code>0) {
+    	    guff_geo.fail();
+        }
+    },
+    
+	fail:function() {
+	    $('#content').hide();
+		$('#geo-fail').slideDown();
+	},
+	
 	init:function() {	
 		
 		//Check for geo capability
 		if (!$('html').hasClass("geolocation"))
 		{
-			$('#content').hide();
-			$('#geo-fail').slideDown();
+			guff_geo.fail();
 			//Do nothing else
 			return;
 		}
 		
-		watchId = navigator.geolocation.watchPosition(geoHandler, errorHandler, {
+		navigator.geolocation.watchPosition(guff_geo.geoHandler, guff_geo.errorHandler, {
 			enableHighAccuracy: true,
 			maximumAge: 0
 		});
@@ -81,10 +80,8 @@ var guff_geo = {
 		if (test)
 		{
 			setTimeout(function(){
-				
-				//
-				geoHandler({coords: {latitude: 1, longitude: 2, accuracy: 12}});
-				}, 3000);
+				guff_geo.geoHandler({coords: {latitude: 1, longitude: 2, accuracy: 70}});
+			}, 3000);
 		}
 	
 	}
