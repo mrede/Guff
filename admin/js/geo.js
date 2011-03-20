5ba4cc1ceb1d234aac16408d0744fe8e7fa1fc4e
Ben Ede <ben.ede@gmail.com>
2011-03-20 18:29:32 +0000

    removed some shit

--- admin/js/geo.js
+++ admin/js/geo.js
@@ -1,9 +1,4 @@
 function geoHandler(location) {
-	/*var message = document.getElementById("message");
-	message.innerHTML ="<img src='http://maps.google.com/staticmap?center=" + location.coords.latitude + "," + location.coords.longitude + "&size=300x200&maptype=hybrid&zoom=16&key=YOURGOOGLEAPIKEY' />";
-	message.innerHTML+="<p>Longitude: " + location.coords.longitude + "</p>";
-	message.innerHTML+="<p>Latitude: " + location.coords.latitude + "</p>";
-	message.innerHTML+="<p>Accuracy: " + location.coords.accuracy + "</p>";*/
 	$("#post_longitude").attr("value", location.coords.longitude);
 	$("#post_latitude").attr("value", location.coords.latitude);
 	$("#post_accuracy").attr("value", location.coords.accuracy);
