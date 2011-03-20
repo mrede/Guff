5242844c664929f9fedc629e03fe776fae40607d
Tom Burton <tomburton@Raoul-Duke.local>
2011-03-20 15:28:07 +0000

    added in add to home screen functionality

--- apps/frontend/templates/layout.php
+++ apps/frontend/templates/layout.php
@@ -7,11 +7,13 @@
     <link rel="shortcut icon" href="/favicon.ico" />
     <?php #include_stylesheets() ?>
 	<link rel="stylesheet" href="http://code.jquery.com/mobile/1.0a3/jquery.mobile-1.0a3.min.css" />
-	<link rel="stylesheet" href="css/guff.css" />
+	<link rel="stylesheet" href="/css/guff.css" />
+	<link rel="stylesheet" href="/css/add2home.css">
 	<script src="http://code.jquery.com/jquery-1.5.min.js" type="text/javascript"></script>
 	<script src="/js/modernizr-1.6.min.js" type="text/javascript"></script>
 	<script src="/js/geo.js" type="text/javascript"></script>
 	<script src="http://code.jquery.com/mobile/1.0a3/jquery.mobile-1.0a3.min.js" type="text/javascript"></script>
+	<script type="application/javascript" src="/js/add2home.js"></script>
   </head>
   <body>
 	<?php echo $sf_content ?>
