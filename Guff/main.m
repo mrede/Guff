ec737d8836b47b09cfb34a3de67c33a3a752e140
Tom Burton <tomburton@Raoul-Duke.local>
2012-06-01 13:52:51 +0100

    initial phone gap project template

---
+++ Guff/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  Guff
+//
+//  Created by Tom Burton on 01/06/2012.
+//  Copyright Beef 2012. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+int main(int argc, char *argv[]) {
+    
+    NSAutoreleasePool * pool = [[NSAutoreleasePool alloc] init];
+    int retVal = UIApplicationMain(argc, argv, nil, @"AppDelegate");
+    [pool release];
+    return retVal;
+}
